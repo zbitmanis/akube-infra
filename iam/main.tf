f3be21bd5b232a0aca30677515d876f0a7e28d80
andris <andris@z-b.lv>
2022-06-19 22:02:16 +0300

    feat: updated IAM policies for AWS LB controller and ECR

--- iam/main.tf
+++ iam/main.tf
@@ -22,7 +22,9 @@ data "aws_iam_policy_document" "dynamo_policy" {
 locals {
   iam_name_prefix = "akube-infra-ec2"
   iam_policy_name = "${local.iam_name_prefix}-policy"
+  iam_alb_policy_name = "${local.iam_name_prefix}-alb-policy"
   iam_role_name   = "${local.iam_name_prefix}-role"
+  iam_alb_policy_file = "files/alb_iam_policy.json"
 }
 
 module "iam_policy" {
@@ -40,6 +42,22 @@ module "iam_policy" {
   }
 }
 
+module "iam_policy_aws_lb" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
+  version = "v5.1.0"
+
+  name        = local.iam_alb_policy_name
+  path        = "/"
+  description = "Policy for custom K8s cluster node"
+
+  policy = file("${path.module}/${local.iam_alb_policy_file}")
+
+  tags = {
+    PolicyDescription = "Policy managed by TF for ec2 node: aws-load-balancer-controller from https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.2/docs/install/iam_policy.json"
+  }
+}
+
+
 ########################################
 # IAM role and ec2 profile
 #######################################
@@ -56,7 +74,8 @@ module "iam_assumable_role_ec2" {
   create_instance_profile = true
 
   custom_role_policy_arns = [
-    module.iam_policy.arn
+    module.iam_policy.arn,
+    module.iam_policy_aws_lb.arn
   ]
   role_name         = local.iam_role_name
   role_requires_mfa = false
--- vars/iam.tfvars
+++ vars/iam.tfvars
@@ -2,5 +2,14 @@ iam_ec2_policy_statemnts = [
   { "sid" = "DynamoDBAccessForClusterJoin",
     "actions" = ["dynamodb:*"],
     "resources" = ["*"]
+  },
+  { "sid" = "ECRReadOnly",
+    "actions" = [
+    "ecr:BatchCheckLayerAvailability",
+    "ecr:BatchGetImage",
+    "ecr:GetDownloadUrlForLayer",
+    "ecr:GetAuthorizationToken"
+    ],
+    "resources" = ["*"]
   }
 ]
