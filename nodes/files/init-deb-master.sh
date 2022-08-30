2b72acbf4d6f8cad62f77b3edd001343181651b7
andris <andris@z-b.lv>
2022-08-30 08:33:34 +0300

    feat: updated user data - pip

--- nodes/files/init-deb-master.sh
+++ nodes/files/init-deb-master.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 apt update
 apt -y install python3-pip git 
-pip3 install ansible 
 mkdir -p /var/tmp/ansible/
 cd /var/tmp/ansible/
 git clone https://github.com/zbitmanis/kube-node.git
 cd /var/tmp/ansible/kube-node
+pip3 install -r requirements.txt
 ansible-galaxy install -r requirements.yml
 [ -d "/var/tmp/ansible/kube-node/group_vars/" ] || mkdir -p /var/tmp/ansible/kube-node/group_vars/
 cat <<EOF > /var/tmp/ansible/kube-node/group_vars/k8s.yaml
 install_kubernetes_node_type: master
+install_kubernetes_node_cni: vpc
 EOF
 cd /var/tmp/ansible/
 git clone https://github.com/zbitmanis/k8sautojoin.git
